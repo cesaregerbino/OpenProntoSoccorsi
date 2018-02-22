a87077ade2ab32a6183bb6b13eda8d63ba9b1b93
Cesare Gerbino <cesare.gerbino@gmai.com>
2018-02-22 20:39:06 +0000

    First complete version of testProntoSoccorsi.sh ...

--- Tools/testProntoSoccorsi.sh
+++ Tools/testProntoSoccorsi.sh
@@ -1,8 +1,18 @@
-echo "Test Pronto Soccorso Genova ..."
-curl http://localhost/OpenProntoSoccorso/API/getProntoSoccorsoDetailsByMunicipality.php?municipality=Genova > /dev/null
-sleep 30
-echo "Test Pronto Soccorso Torino ..."
-curl http://localhost/OpenProntoSoccorso/API/getProntoSoccorsoDetailsByMunicipality.php?municipality=Torino > /dev/null
-sleep 30
-echo "Test Pronto Soccorso Alghero ..."
-curl http://localhost/OpenProntoSoccorso/API/getProntoSoccorsoDetailsByMunicipality.php?municipality=Alghero > /dev/null
+
+#***************************************************************************************************
+#*** Open Pronto Soccorso - Test on avaliable Italian First Aids
+#*** Description: Invoke by curl for each Italian municipality where there are First Aids locations
+#***              The municipalities are in the ElencoComuni.txt file
+#***        Note: The ElencoComuni.txt file does NOT contains the all Italian First Aids, but the only ones for which
+#***              the waiting list numbers for each code white, green, yellow and red ones) are available
+#***              in open data (services) or in some HTML web portal pages
+#***      Author: Cesare Gerbino
+#***        Code: https://github.com/cesaregerbino/OpenProntoSoccorso
+#***     License: MIT (https://opensource.org/licenses/MIT)
+#***************************************************************************************************
+
+while read comune; do
+  echo "Test Pronto Soccorso del comune: " $comune
+  curl http://localhost/OpenProntoSoccorso/API/getProntoSoccorsoDetailsByMunicipality.php?municipality=${comune// /%20} > /dev/null
+  sleep 5
+done <ElencoComuni.txt
